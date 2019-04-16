8cfe6db86a918822a83b756665c16fff02c0c8be
Ian Allheim <iallheim@datto.com>
2019-04-16 13:19:22 -0400

    All done!

--- scripting.sh
+++ scripting.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-if [ $? -lt '1' ] ; then
+if [ $hosts -lt 1 ] ;
+
+then
     echo "You didn't add any arguments. Exiting !"
     exit 1
 
 else
 
-hosts = "yahoo.com google.com amazon.com $1"
+hosts="yahoo.com google.com amazon.com"
 
 for website in $hosts ; do ping -c3 "$website"; done
 
+echo "This is for you"
 
+curl parrot.live & sleep 5 ; kill $!
+
+echo "Goodbye"
+
+fi
